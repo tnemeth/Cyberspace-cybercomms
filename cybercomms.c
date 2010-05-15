a6e016ce1c37455990f02d8811e7107ccb8b1a9a
Thomas Nemeth <thomas@cixi.(none)>
2010-05-15 16:33:31 +0200

    Fixed missing documentation.

--- cybercomms.c
+++ cybercomms.c
@@ -1,10 +1,11 @@
 /**
  *  \file    cybercomms.c
- *  \brief   .
+ *  \brief   Cyberspace communications.
  *
  *           Project: cybercomms (Cyberspace communication library).
  *
- *           File description here...
+ *           This file contains the wrapping functions for cyberspace
+ *           communications.
  *
  *  \author  Thomas Nemeth
  *
@@ -22,6 +23,23 @@
 #include "cyberspace.h"
 
 
+/**
+ *  \brief Cyberspace connection function.
+ *
+ *         This function connects a client to the cyberspace system server.
+ *
+ * @param machine               machine hosting the cyberspace system server
+ * @param port                  port of the listening server
+ * @param user                  type of client/user
+ * @return                      the communication socket or an error status
+ * @retval -ERR_CONNECT_SERVER          unable to connect to the server
+ * @retval -ERR_BAD_PARAMETER           remote host no given in parameter
+ * @retval -ERR_SERVER_INFO             could not find server information
+ * @retval -ERR_CREATE_SOCKET           could not create the socket
+ * @retval -ERR_CONFIGURE_SOCKET        could not configure the socket
+ * @retval -ERR_UNKNOWN_ADDRESS         could not find address (host)
+ * @retval -ERR_BIND_SOCKET             could not bind the socket
+ */
 int cyberspace_connect(const char * machine, int port, client_type user)
 {
         int           sock = connect_server(machine, port);

fe302cc8e6f7d220be46219eeb30fb79b22a6d9c
Thomas Nemeth <thomas@cixi.(none)>
2010-05-15 16:37:44 +0200

    Added cyberspace main header.

---
+++ cyberspace.h
@@ -0,0 +1,41 @@
+/**
+ *  \file    cyberspace.h
+ *  \brief   Cyberspace main header..
+ *
+ *           Project: cybercomms (Cyberspace communication library).
+ *
+ *           This file contains all that is needed for a client to connect
+ *           to the cyberspace system server.
+ *
+ *  \author  Thomas Nemeth
+ *
+ *  \version 1.0.0
+ *  \date    Sat, May 15 2010
+ */
+
+
+#ifndef CYBERSPACE_H
+#define CYBERSPACE_H
+
+#include "errors.h"
+#include "sockets.h"
+#include "packets.h"
+#include "tags.h"
+
+/**
+ *  \brief cyty Cyberspace communication data types.
+ *  @{
+ */
+
+/*! Types of client that can connect to the cyberspace system server. */
+typedef enum {client_god, client_probe, client_ship} client_type;
+
+/** @} */
+
+
+/** @cond DUPLICATE_DOCUMENTATION */
+int cyberspace_connect(const char * machine, int port, client_type user);
+/** @endcond */
+
+#endif /* CYBERSPACE_H */
+
